110ee7889c70d7c3ae41277cb1704ea504ee1a69
Spencer Wildes <spencerwildes@gmail.com>
2018-01-18 17:37:55 -0800

    init commit

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Single Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Aloha Burger', true);
+
+
